7e37c72eeb5bf925e051949ae88a6223480b2e51
Kevin <99073020@mydavinci.nl>
2022-09-08 11:44:12 +0200

    person

--- module_02-omgaan_met/deel_01/feestlunch.py
+++ module_02-omgaan_met/deel_01/feestlunch.py
@@ -6,10 +6,6 @@ aantalcroissant = 17
 aantalstokbrood = 2
 aantalkortingsbon = 3
 
-croissant = float(croissant)
-stokbrood = float(stokbrood)
-kortingsbon = float(kortingsbon)
-
 betalen = (aantalcroissant * croissant + aantalstokbrood * stokbrood) - (aantalkortingsbon * kortingsbon)
    
 print("De feestlunch kost je bij elkaar", betalen, "euro voor de", aantalcroissant, "croissantjes en de", aantalstokbrood, "stokbroden als de", aantalkortingsbon, "kortingsbonnen nog geldig zijn!")
---
+++ module_02-omgaan_met/deel_02/person.py
@@ -0,0 +1,13 @@
+name = input('Hoe heet je?')
+adres = input('Wat is je je adres?')
+postcode = input('Wat is je postcode?')
+woonplaats = input('Waar woon je?')
+
+print("""
+ ----------------------------------------------------
+| Naam:""", name, """
+| Adres:""", adres, """"
+| Postcode:""", postcode, """
+| Woonplaats: """, woonplaats, """
+ ----------------------------------------------------
+""")
