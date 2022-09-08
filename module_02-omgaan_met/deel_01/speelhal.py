22fa2664bb33aaa3331ec4a7b30126fa346b101c
Kevin <99073020@mydavinci.nl>
2022-09-08 10:26:09 +0200

    speelhal

---
+++ module_02-omgaan_met/deel_01/speelhal.py
@@ -0,0 +1,15 @@
+# speelhal
+
+aantalpersonen = 3
+
+ticket = 7.45
+ticket = float(ticket)
+
+gameseat = 0.37
+gameseat = float(gameseat)
+aantalminuten = 5
+keerminuten = 9
+
+betalen = (aantalpersonen * ticket) + (keerminuten * gameseat)
+
+print('Te betalen:', betalen)
