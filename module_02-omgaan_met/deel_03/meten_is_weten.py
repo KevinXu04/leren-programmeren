3e0c107d178f612e0ee8d947c8af4038687ffbda
Kevin <99073020@mydavinci.nl>
2022-09-09 09:43:09 +0200

    Min en Max

--- module_02-omgaan_met/deel_03/meten_is_weten.py
+++ module_02-omgaan_met/deel_03/meten_is_weten.py
@@ -3,9 +3,15 @@ b = int(input("Kies een getal"))
 
 if a > b:
     max = a
+    min = b
     print("a is het grootste getal:", max)
+    print("Het minimum is:", b)
+    print("Het maximum is:", a)
 elif a < b:
     min = a
+    max = b
     print("a is het kleinste getal:", min)
+    print("Het minimum is:", a)
+    print("Het maximum is:", b)
 else :
     print("a is gelijk aan b")
