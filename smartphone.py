c1c85a414b95ecccf28bdcb47817c6f421daf670
Kevin <99073020@mydavinci.nl>
2022-09-22 10:10:24 +0200

    smartphone controle

--- smartphone.py
+++ smartphone.py
@@ -4,6 +4,20 @@ samsung = int(input("Hoe duur is de Samsung Galaxy S22? "))
 max1 = samsung - iphone
 max2 = iphone - samsung
 
+if (iphone > 900 and samsung > 900 and iphone > samsung):
+    print(f"""De iPhone is het duurst, de telefoon kost: {iphone} euro.
+De Samsung is het goedkoopst, de telefoon kost: {samsung} euro
+Het advies is dus geen telefoon te kopen, ze zijn te duur
+""")
+    exit()
+elif (iphone > 900 and samsung > 900 and iphone < samsung):
+    print(f"""De Samsung is het duurst, de telefoon kost: {samsung} euro.
+De iPhone is het goedkoopst, de telefoon kost: {iphone} euro
+Het advies is dus geen telefoon te kopen, ze zijn te duur
+""")
+    exit()
+
+
 if iphone < samsung:
     print(f"""De Samsung is het duurst, de telefoon kost: {samsung} euro.
 De iPhone is het goedkoopst, de telefoon kost: {iphone} euro.
