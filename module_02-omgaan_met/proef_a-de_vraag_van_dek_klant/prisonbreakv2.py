911b2b93dbb3b19937b01f95bce84a63f2c2198b
Kevin <99073020@mydavinci.nl>
2022-09-27 11:40:33 +0200

    AdventureGame

--- module_02-omgaan_met/proef_a-de_vraag_van_dek_klant/prisonbreakv2.py
+++ module_02-omgaan_met/proef_a-de_vraag_van_dek_klant/prisonbreakv2.py
@@ -5,24 +5,20 @@ def delay_print1 (s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.03)
+        time.sleep(0.01)
 
 delay_print1("Wat is je naam? ")
 name = input().strip()
-delay_print1(f"""Welkom bij Prison Break! {name}!
-Je had een bank beroofd op 1 mei 2019 met 50 miljoen euro aan goud. 2 weken later werd je opgepakt door de politie, omdat je niet goed had verstopt.
-Nu zit je in een van de meest beveiligde gevangenis van de wereld genaamd "ADX Florence" een van de meest beruchte gevangenis. Je kreeg 25 jaar straf in de gevangenis.
-6 maanden later heb je een eigen bende gevormd genaamd {name}'s Gang en ben je van plan om te ontsnappen.
-Nu op 15 november heb je je hele bende bij elkaar en nu moet je een keuze maken van hoe je moet ontsnappen.
+delay_print1(f""".
 """) # Intro
 
-time.sleep(3) # 2 seconden delay
+time.sleep(1) # 3 seconden delay
 
 delay_print1("""
 Loading...
 """)
 
-time.sleep(7)
+time.sleep(1) # 7 seconden 
 
 delay_print1("""
 Je bende heeft in totaal 5 mensen inclusief jij. Ze heten Vincent, Farhan, Oscar en Jez
@@ -37,110 +33,6 @@ Type een van de nummers in.
 
 escape = input().strip()
 
-if escape == "1": # Als je tunnel graven koos
-    delay_print1(f"""
-Op 16 november 2019 zijn alle bendeleden verzameld om te ontsnappen.
-{name}: 'Hey Farhan komen de bewakers?'
-Farhan: 'Nee man, maar ze zijn nu te alert om te ontsnappen en er lopen teveel rond.'
-
-FLASHBACK 1 MAAND GELEDEN
-
-*ALARM LOCKDOWN* *SIRENE GELUID*
-
-Random Prisoner: 'HEY *Random Prisoner 2* HEB JE ZE?'
-Random Prisoner 2: 'NEE MAN ZE ZIJN MET TEVEEL!'
-
-*SHOTS FIRED*
-
-Snap back to reality!
-
-Vincent: 'De gevangene die wouden ontsnappen zijn door de bewakers allemaal dood geschoten.'
-Oscar: 'We hebben een keuze om te wachten totdat ze wat minder alert zijn of we gaan nu!'""")
-
-    delay_print1("""
-Kies nu de optie om te wachten of niet.
-    1. Doorgaan
-    2. Wachten
-Type een van de nummers in.
-""")
-
-    keuze1 = input().strip()
-    if keuze1 == "1": # doodgaan
-        delay_print1(f"""
-Je hebt gekozen om door te gaan.
-Je hebt de tunnel eindelijk gegraven, maar toen je bijna bij het einde was was de tunnel ingestort. 
-Door de ingestorte tunnel gingen {name}, Farhan en Jez levend begraven. De rest werden snel opgepakt door de bewakers
-Helaas heb je de verkeerde keuze genomen en ben je doodgegaan. Probeer het opnieuw!""")
-        exit()
-
-    if keuze1 == "2": # wachten
-        delay_print1(f"""
-Je hebt gekozen om te wachten
-Het is nu 23 januari 2020
-{name}: 'Het is nu eindelijk tijd om te ontsnappen!'
-{name}: 'Vincent en Farhan staan op uitkijk en leidt ze af als de bewakers komen! De rest komen mee om de tunnel verder te graven.'
-Na 1 uur graven ben je eindelijk klaar met de tunnel, maar nu is de vraag ga jij Vincent en Farhan achterlaten om zelf met de andere te ontsnappen?
-Of wacht je tot morgen dat je met z'n allen gaat maar er is risico dat de tunnel ontdekt wordt...""")
-
-        delay_print1("""
-Ga je tot morgen wachten of laat je ze in de steek?
-    1. Wachten
-    2. In de steek laten
-Type een van de nummers in.
-""")
-        keuze2 = input().strip()
-    
-    if keuze2 == "1": # wachten 
-        delay_print1(""""Je hebt gekozen om te wachten tot morgen. *+10 risico*
-Terwijl je aan het slapen bent hoor je opeens geluiden buiten je cell...
-ZE CHECKEN ELKE CELL! NU HEB JE DE KEUZE OM BETRAPT TE WORDEN OF NU ONTSNAPPEN ZONDER JE BENDE!""")
-
-        delay_print1("""
-Laat je je cell check door de bewakers of ontsnap je nu door de tunnel?
-    1. Ontsnap
-    2. Wacht
-Type een van de nummers in.
-""")
-        prisoncheck = input().strip()
-
-    if prisoncheck == "1":
-        delay_print1("""Terwijl je door de tunnel was aan het ontsnappen waren de bewakers al bij je cell. Ze waren sneller dan dat je dacht
-Toen je uit de tunnel was waren ze al op alert dat je weg was. De snipers zagen je en schoten je dood...
-Helaas heb je de verkeerde keuze genomen en ben je doodgegaan. Probeer het opnieuw!""")
-        exit()
-    if prisoncheck == "2":
-        delay_print1("""Je hebt gekozen om te wachten...
-Jij bent nu aan de beurt...
-Ze jouw cell aan het doorzoeken...
-...
-Helaas hebben ze de tunnel gevonden... Probeer het opnieuw!""")
-        exit()
-    if keuze2 == "2":
-        delay_print1("""Je hebt gekozen om je kameraden in de steek te laten
-...
-Toen jullie eindelijk vrij waren hadden Oscar en Jez achtergebleven om te wachten voor Vincent en Farhan.""")
-
-        delay_print1("""
-Nu heb je de keuze om te wachten of alleen verder te gaan. Type het getal in.
-    1. Wachten
-    2. Verder gaan
-Type een van de nummers in.
-        """)
-        keuze3 = input().strip()
-        if keuze3 == "1":
-            delay_print1("""Je hebt gekozen om te wachten.
-Helaas was dat de foute keuze geweest. Na een tijdje hadden de bewakers je gespot.
-Jullie probeerden om weg te rennen, maar het lukte niet. Uiteindelijk waren jullie alle drie doodgeschoten.
-Helaas heb je de verkeerde keuze genomen en ben je doodgegaan. Probeer het opnieuw!""")
-            exit()
-        if keuze3 == "2":
-            delay_print1("""Je hebt gekozen om alleen verder te gaan
-...
-Gefeliciteerd je bent ontsnapt!
-Helaas waren de rest van je bende opgepakt.
-THE END
-""")
-
 if escape == "2": # gekozen voor prison shootout
     delay_print1(f"""Je hebt gekozen om de harde manier te nemen.
 {name}: 'Hey Oscar heb je de wapens uitgedeeld?'
@@ -151,7 +43,7 @@ Nu heb je de keuze om nu te rellen of om te wachten. Type het nummer in.
     1. Verder gaan
     2. Wachten
 Type een van de nummers in.
-    """)
+""")
     keuze4 = input().strip()
 
     if keuze4 == "1":
@@ -218,3 +110,109 @@ Een van de mensen hadden jou uitverkocht aan de bewakers.
 Nu checken ze elke cell voor wapens...
 Helaas ben je betrapt door de bewakers. Probeer het opnieuw!""")
         exit()
+
+
+if escape == "1": # Als je tunnel graven koos
+    delay_print1(f"""
+Op 16 november 2019 zijn alle bendeleden verzameld om te ontsnappen.
+{name}: 'Hey Farhan komen de bewakers?'
+Farhan: 'Nee man, maar ze zijn nu te alert om te ontsnappen en er lopen teveel rond.'
+
+FLASHBACK 1 MAAND GELEDEN
+
+*ALARM LOCKDOWN* *SIRENE GELUID*
+
+Random Prisoner: 'HEY *Random Prisoner 2* HEB JE ZE?'
+Random Prisoner 2: 'NEE MAN ZE ZIJN MET TEVEEL!'
+
+*SHOTS FIRED*
+
+Snap back to reality!
+
+Vincent: 'De gevangene die wouden ontsnappen zijn door de bewakers allemaal dood geschoten.'
+Oscar: 'We hebben een keuze om te wachten totdat ze wat minder alert zijn of we gaan nu!'""")
+
+    delay_print1("""
+Kies nu de optie om te wachten of niet.
+    1. Doorgaan
+    2. Wachten
+Type een van de nummers in.
+""")
+    keuze1 = input().strip()
+    
+    if keuze1 == "1": # doodgaan
+        delay_print1(f"""
+Je hebt gekozen om door te gaan.
+Je hebt de tunnel eindelijk gegraven, maar toen je bijna bij het einde was was de tunnel ingestort. 
+Door de ingestorte tunnel gingen {name}, Farhan en Jez levend begraven. De rest werden snel opgepakt door de bewakers
+Helaas heb je de verkeerde keuze genomen en ben je doodgegaan. Probeer het opnieuw!""")
+    exit()
+
+if keuze1 == "2": # wachten
+    delay_print1(f"""
+Je hebt gekozen om te wachten
+Het is nu 23 januari 2020
+{name}: 'Het is nu eindelijk tijd om te ontsnappen!'
+{name}: 'Vincent en Farhan staan op uitkijk en leidt ze af als de bewakers komen! De rest komen mee om de tunnel verder te graven.'
+Na 1 uur graven ben je eindelijk klaar met de tunnel, maar nu is de vraag ga jij Vincent en Farhan achterlaten om zelf met de andere te ontsnappen?
+Of wacht je tot morgen dat je met z'n allen gaat maar er is risico dat de tunnel ontdekt wordt...""")
+
+    delay_print1("""
+Ga je tot morgen wachten of laat je ze in de steek?
+    1. Wachten
+    2. In de steek laten
+Type een van de nummers in.
+""")
+    keuze2 = input().strip()
+    
+if keuze2 == "1": # wachten 
+    delay_print1(""""Je hebt gekozen om te wachten tot morgen. *+10 risico*
+Terwijl je aan het slapen bent hoor je opeens geluiden buiten je cell...
+ZE CHECKEN ELKE CELL! NU HEB JE DE KEUZE OM BETRAPT TE WORDEN OF NU ONTSNAPPEN ZONDER JE BENDE!""")
+
+    delay_print1("""
+Laat je je cell check door de bewakers of ontsnap je nu door de tunnel?
+    1. Ontsnap
+    2. Wacht
+Type een van de nummers in.
+""")
+    prisoncheck = input().strip()
+
+if prisoncheck == "1":
+        delay_print1("""Terwijl je door de tunnel was aan het ontsnappen waren de bewakers al bij je cell. Ze waren sneller dan dat je dacht
+Toen je uit de tunnel was waren ze al op alert dat je weg was. De snipers zagen je en schoten je dood...
+Helaas heb je de verkeerde keuze genomen en ben je doodgegaan. Probeer het opnieuw!""")
+        exit()
+if prisoncheck == "2":
+        delay_print1("""Je hebt gekozen om te wachten...
+Jij bent nu aan de beurt...
+Ze jouw cell aan het doorzoeken...
+...
+Helaas hebben ze de tunnel gevonden... Probeer het opnieuw!""")
+        exit()
+if keuze2 == "2":
+        delay_print1("""Je hebt gekozen om je kameraden in de steek te laten
+...
+Toen jullie eindelijk vrij waren hadden Oscar en Jez achtergebleven om te wachten voor Vincent en Farhan.""")
+
+        delay_print1("""
+Nu heb je de keuze om te wachten of alleen verder te gaan. Type het getal in.
+    1. Wachten
+    2. Verder gaan
+Type een van de nummers in.
+        """)
+        keuze3 = input().strip()
+        if keuze3 == "1":
+            delay_print1("""Je hebt gekozen om te wachten.
+Helaas was dat de foute keuze geweest. Na een tijdje hadden de bewakers je gespot.
+Jullie probeerden om weg te rennen, maar het lukte niet. Uiteindelijk waren jullie alle drie doodgeschoten.
+Helaas heb je de verkeerde keuze genomen en ben je doodgegaan. Probeer het opnieuw!""")
+            exit()
+        if keuze3 == "2":
+            delay_print1("""Je hebt gekozen om alleen verder te gaan
+...
+Gefeliciteerd je bent ontsnapt!
+Helaas waren de rest van je bende opgepakt.
+THE END
+""")
+
