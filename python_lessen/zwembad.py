6e16f52930c12f3b59479aa950dc45bcdf467441
Kevin <99073020@mydavinci.nl>
2022-10-13 10:49:23 +0200

    zwembad stap 5b

--- python_lessen/zwembad.py
+++ python_lessen/zwembad.py
@@ -23,28 +23,59 @@ voorrijkosten = kostenPerKM + voorrijkostenVastePrijs
 zwembadVierkanteMeter = lengte * breedte
 
 # Afwerken kosten
+roodMeerprijs = input("Wilt u rode kleur tegels? Y/N ").lower()
+keuzeKleurMeerprijs = input("Wilt u een andere kleur tegels? Y/N ").lower()
+
 if zwembadInhoud < 20:
     afwerkenPrijsPer = 250
     meerprijsRood = 25
     meerprijsKeuzeKleur = 100
+    afwerkenkostenNormal = zwembadVierkanteMeter * afwerkenPrijsPer
+    if roodMeerprijs == "y":
+        afwerkenkostenRood = zwembadVierkanteMeter * meerprijsRood
+    elif roodMeerprijs == "n":
+        meerprijsRood = 0
+        afwerkenkostenRood = zwembadVierkanteMeter * meerprijsRood
+    if keuzeKleurMeerprijs == "y":
+        afwerkenkostenKeuze = zwembadVierkanteMeter * meerprijsKeuzeKleur
+    elif keuzeKleurMeerprijs == "n":
+        meerprijsKeuzeKleur = 0
+        afwerkenkostenKeuze = zwembadVierkanteMeter * meerprijsKeuzeKleur
+
+
 elif zwembadInhoud >= 20:
     afwerkenPrijsPer = 200
     meerprijsRood = 20
     meerprijsKeuzeKleur = 125
+    afwerkenkostenNormal = zwembadVierkanteMeter * afwerkenPrijsPer
+    if roodMeerprijs == "y":
+        afwerkenkostenRood = zwembadVierkanteMeter * meerprijsRood
+    elif roodMeerprijs == "n":
+        meerprijsRood = 0
+        afwerkenkostenRood = zwembadVierkanteMeter * meerprijsRood
+    if keuzeKleurMeerprijs == "y":
+        afwerkenkostenKeuze = zwembadVierkanteMeter * meerprijsKeuzeKleur
+    elif keuzeKleurMeerprijs == "n":
+        meerprijsKeuzeKleur = 0
+        afwerkenkostenKeuze = zwembadVierkanteMeter * meerprijsKeuzeKleur
+
 else:
     print("Error")
     exit()
 
+afwerkkostenTotaal = afwerkenkostenNormal + afwerkenkostenRood + afwerkenkostenKeuze
+
 # Totaal kosten
-totaalprijs = afvoerGrondPrijs + uitgravenPrijs + voorrijkosten
+totaalprijs = afvoerGrondPrijs + uitgravenPrijs + voorrijkosten + afwerkkostenTotaal
 
 
 
 # Bon
 print(f"""
-Uitgraven:          {uitgravenPrijs}
-Afvoeren grond:     {afvoerGrondPrijs}
-Voorrijkosten       {voorrijkosten}
+Uitgraven:              {uitgravenPrijs}
+Afvoeren grond:         {afvoerGrondPrijs}
+Voorrijkosten           {voorrijkosten}
+Beton + tegel {zwembadVierkanteMeter}m2      {afwerkkostenTotaal}
 
-Totaal prijs:       {totaalprijs}
+Totaal prijs:           {totaalprijs}
 """)
