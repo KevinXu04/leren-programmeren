ae9dfc60dd4b1c8ea5f1cd5aa7a6cf63ba4bddd8
Kevin <99073020@mydavinci.nl>
2022-10-13 11:12:58 +0200

    dagen tot dan

--- module_03-keer_op_keer/deel_02/dagenTotDan.py
+++ module_03-keer_op_keer/deel_02/dagenTotDan.py
@@ -1,6 +1,9 @@
 currentDay = input("What day is it? ")
 
 week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
+x = 0
 while True:
-    
+    print(week[x])
+    if week[x] == currentDay:
+        break
+    x += 1
