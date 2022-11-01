073958dcc603c84719ae7fded8f2601b0035deeb
Kevin <99073020@mydavinci.nl>
2022-11-01 11:49:44 +0100

    coins

--- module_03-keer_op_keer/deel_02/coins.py
+++ module_03-keer_op_keer/deel_02/coins.py
@@ -4,36 +4,51 @@
 # function of program:
 # structure of program: 
 
-toPay = int(float(input('Amount to pay: '))* 100) #
-paid = int(float(input('Paid amount: ')) * 100) #
-change = paid - toPay #
+toPay = int(float(input('Amount to pay: '))) # Hij vraagt hier hoeveel hij moet betalen.
+paid = int(float(input('Paid amount: '))) # Hoeveel geld hij gegeven heeft.
+change = paid - toPay # Hoeveel hij terug moet terug geven.
 
-if change > 0: #
-  coinValue = 50 #
+if change > 0: # Als hij wat terug moet geven boven de 0 is voert hij de code hieronder uit.
+  coinValue = 5 # Hier begint hij coinValue met 5 euro.
+  returnTicket = ''
   
-  while change > 0 and coinValue > 0: #
-    nrCoins = change // coinValue #
+  while change > 0 and coinValue > 0: # Hij voert een while loop uit als wat hij terug moet geven boven 0 is en coinValue boven 0.
+    nrCoins = change // coinValue # Hier berekent hij hoeveel munten hij maximaal mag terug geven.
 
-    if nrCoins > 0: #
-      print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) #
-      nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #
-      change -= nrCoinsReturned * coinValue #
+    if nrCoins > 0: # Als het aantal munten hoger dan 0 is voert hij de code hieronder uit.
+      print('return maximal', nrCoins, 'coins of', coinValue, 'euro!' ) # Hier print hij het maximaal aantal munten van bijvoorbeeld 50 cent dat je kan terug geven.
+      nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' euro did you return? ')) # Hier vraagt hij hoeveel munten van een soort dat je terug hebt gegeven.
+      change -= nrCoinsReturned * coinValue # Hier berekent hij hoeveel je nog moet terug geven.
 
-# comment on code below: 
-    if coinValue == 50:
-      coinValue = 20
-    elif coinValue == 20:
-      coinValue = 10
-    elif coinValue == 10:
-      coinValue = 5
-    elif coinValue == 5:
+      returnTicket += str(nrCoinsReturned)
+      returnTicket += 'x '
+      returnTicket += str(coinValue)
+      returnTicket += ' euro munt teruggegven\n'
+
+# comment on code below:
+    if coinValue == 5:
       coinValue = 2
     elif coinValue == 2:
       coinValue = 1
+    elif coinValue == 1:
+      coinValue = 0.50 
+    elif coinValue == 0.50:
+      coinValue = 0.20
+    elif coinValue == 0.20:
+      coinValue = 0.10
+    elif coinValue == 0.10:
+      coinValue = 0.05
+    elif coinValue == 0.05:
+      coinValue = 0.02
+    elif coinValue == 0.02:
+      coinValue = 0.01
     else:
       coinValue = 0
+elif change <= 0:
+  print("Je hebt niet genoeg gegeven. Probeer opnieuw!")
 
-if change > 0: #
-  print('Change not returned: ', str(change) + ' cents')
-else:
-  print('done')
+
+if change > 0: # Als je nog niet alles terug hebt gegeven print hij de code eronder van hoeveel hij niet terug gaf. Als je wel alles terug gaf print hij welke munten je terug gaf.
+  print('Change not returned: ', str(change) + ' euro')
+elif change == 0:
+  print(returnTicket)
--- module_03-keer_op_keer/deel_02/while_loop_test.py
+++ module_03-keer_op_keer/deel_02/while_loop_test.py
@@ -1,9 +1,7 @@
-i = int(input("Geef een getal. "))
-
-while i <= 10:
-    print(f"Het getal is kleiner of gelijk aan 10. Het getal is {i}")
-    break
-
-while i > 10:
-    print(f"Het getal is groter dan 10 dus. Het getal is {i}")
-    break
+while True:
+    x = input("Geef een getal")
+    if x == "6":
+        print(x)
+        break
+    else:
+        print("Een getal!")
