2a2a2bc19b822d0dea6277b56f9c2715f614ee3a
Kevin <99073020@mydavinci.nl>
2022-11-04 12:01:02 +0100

    robotarm13

--- module_03-keer_op_keer/deel_01/robotarm-python-2021-main-kevin/example.py
+++ module_03-keer_op_keer/deel_01/robotarm-python-2021-main-kevin/example.py
@@ -1,23 +1,25 @@
 from shutil import move
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 12')
+robotArm = RobotArm()
+robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 2
 teller = 9
-for x in range(0,9):
+bewegen = True
+while bewegen:
     robotArm.grab()
     color = robotArm.scan()
-    if color == "red":
+    if color == "":
+        bewegen = False
+        robotArm.wait()
+    else:
         for x in range(0,teller):
             robotArm.moveRight()
         robotArm.drop()
         for x in range(0,teller):
             robotArm.moveLeft()
-    else:
-        robotArm.drop()
-    robotArm.moveRight()
-    teller -=1
+        teller -=1
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
