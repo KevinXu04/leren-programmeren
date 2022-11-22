0624f62bef9bdf6d3abcffc2e4050a73381d7979
Kevin <99073020@mydavinci.nl>
2022-11-22 09:40:37 +0100

    deckv2

--- module_04-lijstjes_en_samenstellingen/deel_01/deckSpeelkaarten.py
+++ module_04-lijstjes_en_samenstellingen/deel_01/deckSpeelkaarten.py
@@ -1,14 +1,16 @@
 import random
 values = ['2','3','4','5','6','7','8','9','10','Boer','Vrouw','Heer','Aas']
 suites = ['Harten', 'Klaver', 'Schoppen', 'Ruiten']
-joker = ['Joker']
-joker2 = ['Joker']
+rood = ('Rode', 'Joker')
+zwart = ('Zwarte', 'Joker')
 deck = [[s, v] for s in suites for v in values]
-deck.append(joker and joker2)
+deck.append(rood and zwart)
 random.shuffle(deck)
+y= 0
 z = 0
 for x in range(7):
     z += 1
-    print(f"Kaart {z}:", random.choice(deck))
+    print(f"Kaart {z}:", deck[y])
+    y += 1
 
-print("Deck (47 kaarten):", deck)
+print("Deck (47 kaarten):", deck[7:54])
