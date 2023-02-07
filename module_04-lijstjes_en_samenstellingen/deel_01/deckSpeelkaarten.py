4821e1564e4db4e521542270573f13ce1c8edf81
Kevin <99073020@mydavinci.nl>
2023-02-07 12:13:55 +0100

    Lootjes verdeler

--- module_04-lijstjes_en_samenstellingen/deel_01/deckSpeelkaarten.py
+++ module_04-lijstjes_en_samenstellingen/deel_01/deckSpeelkaarten.py
@@ -8,14 +8,11 @@ deck.append("Joker2")
 random.shuffle(deck)
 
 deckAantal = len(deck) - 7
-
+print(deck)
 p = 0
-o = 0
 for x in range(7):
     p += 1
-    print(f"Kaart {p}:", deck[o])
-    del deck[o]
-    o += 1    
-random.shuffle(deck)
+    print(f"Kaart {p}:", deck[0])
+    del deck[0]
 print(f"Deck ({deckAantal} kaarten):", deck)
 # Kaarten uit de deck halen en het aantal kaarten dat overblijft optellen
--- module_04-lijstjes_en_samenstellingen/fruitmand2/opdracht11.py
+++ module_04-lijstjes_en_samenstellingen/fruitmand2/opdracht11.py
@@ -1,28 +1,26 @@
 from fruitmand import fruitmand
 
-kleuren = [item['color'] for item in fruitmand]
+kleuren = [item['color'] for item in fruitmand if item['round'] == False]
 
 print("yellow, green, orange, red, brown\n")
-while True:
+vraagKleur = ''
+while vraagKleur not in kleuren:
     vraagKleur = input("Kies een kleur uit. ")
-    if vraagKleur in kleuren:
-        break
-    else:
+    if vraagKleur not in kleuren:
         print(f"De kleur {vraagKleur} zit er niet in de fruitmand.")
-        continue
 
 rond, nietRond = 0, 0
-for i in range(len(fruitmand)):
-    kleuren = fruitmand[i].get('color')
+for fruit in fruitmand:
+    kleuren = fruit['color']
     if kleuren == vraagKleur:
-        if fruitmand[i].get('round'):
+        if fruit['round']:
             rond += 1
         else:
             nietRond += 1
 
 if rond > nietRond:
-    print(f"Er zijn {rond} meer ronde vruchten dan niet ronde vruchten in de kleur {vraagKleur}")
+    print(f"Er zijn {rond-nietRond} meer ronde vruchten dan niet ronde vruchten in de kleur {vraagKleur}")
 elif rond < nietRond:
-    print(f"Er zijn {rond} minder ronde vruchten dan niet ronde vruchten in de kleur {vraagKleur}")
+    print(f"Er zijn {nietRond-rond} minder ronde vruchten dan niet ronde vruchten in de kleur {vraagKleur}")
 else:
     print(f"Er zijn {rond} ronde vruchten en {nietRond} niet ronde vruchten in de kleur {vraagKleur}")
