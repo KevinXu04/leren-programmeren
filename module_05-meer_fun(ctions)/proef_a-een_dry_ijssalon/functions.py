97d96e62838caf5609775e54028d98ac22adf5a8
Kevin <99073020@mydavinci.nl>
2023-03-16 14:40:39 +0100

    M5ProefA | Een DRY ijssalon | Toppings

--- module_05-meer_fun(ctions)/proef_a-een_dry_ijssalon/functions.py
+++ module_05-meer_fun(ctions)/proef_a-een_dry_ijssalon/functions.py
@@ -23,35 +23,36 @@ def bakjeOfHoorntje(aantal):
         keuze = "bakje"
 
     print(f"Hier is uw {keuze} met {aantal} bolletje(s) ")
-    return aantal, keuze
+    return keuze
 
-def smaakKiezen(aantal):
+def smaakEnTopping(aantal, lst, tekst):
     keuzeBoolean = True
-    smaakLst = [{'name': 'aardbei', 'aantal': 0, 'prijs': 1.10},
-                 {'name': 'chocolade', 'aantal': 0, 'prijs': 1.10},
-                 {'name': 'munt', 'aantal': 0, 'prijs': 1.10},
-                 {'name': 'vanille', 'aantal': 0, 'prijs': 1.10}]
     
     for index in range(aantal):
         while keuzeBoolean:
-            smaakKeuze = input(f"Welke smaak wilt u voor bolletje nummer {index+1}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?").lower()
-            if smaakKeuze == "a":
-                smaakLst[0]['aantal'] += 1
+            keuze = input(tekst).lower()
+            if keuze == 'a':
+                keuze = lst[0]['name']
                 keuzeBoolean = False
-            elif smaakKeuze == "c":
-                smaakLst[1]['aantal'] += 1
+            elif keuze == 'b':
+                keuze = lst[1]['name']
                 keuzeBoolean = False
-            elif smaakKeuze == "m":
-                smaakLst[2]['aantal'] += 1 
+            elif keuze == 'c':
+                keuze = lst[2]['name']
                 keuzeBoolean = False
-            elif smaakKeuze == "v":
-                smaakLst[3]['aantal'] += 1 
+            elif keuze == 'd':
+                keuze = lst[3]['name']
                 keuzeBoolean = False
             else:
                 print("Sorry dat snap ik niet...")
+
+            for item in lst:
+                if keuze == item['name']:
+                    item['aantal'] += 1
+            
         keuzeBoolean = True
+    return lst
         
-    return smaakLst
 
 def meerBestellen(nogEen):
     while True:
@@ -64,11 +65,7 @@ def meerBestellen(nogEen):
         else:
             print("Sorry dat snap ik niet")
 
-def berekening(aantal, keuze):
-    lst = [{'name': 'bolletje', 'aantal': 0, 'prijs': 1.10},
-            {'name': 'hoorntje', 'aantal': 0, 'prijs': 1.25},
-            {'name': 'bakje', 'aantal': 0, 'prijs': 0.75}]
-
+def bolletjesEnKeuzeBerekening(aantal, keuze, lst):
     lst[0]['aantal'] += aantal
 
     for item in lst:
@@ -77,8 +74,9 @@ def berekening(aantal, keuze):
 
     return lst
 
-def bon(lst, smaakLst):
+def bon(lst, smaakLst, toppings):
     totaalPrijs = 0
+    totaalPrijsToppings = 0
     lst.extend(smaakLst)
 
     print('--------["Papi Gelato"]--------')
@@ -86,6 +84,31 @@ def bon(lst, smaakLst):
         if index['aantal']:
             print(f"{index['name']} {index['aantal']} x € {'%.2f' % (index['prijs'])} = € {'%.2f' % (index['aantal'] * index['prijs'])}")
         totaalPrijs += index['aantal'] * index['prijs']
+
+    # if lst[1]['aantal'] > 0:
+    #     toppings[3]['prijs'] = 0.6
+    #     lst[1]['aantal'] = 0
+    #     totaalPrijsToppings += toppings[3]['prijs'] * toppings[3]['aantal']
+    # if lst[2]['aantal'] > 0:
+    #     toppings[3]['prijs'] = 0.9
+    #     lst[2]['aantal'] = 0
+    #     totaalPrijsToppings += toppings[3]['prijs'] * toppings[3]['aantal']
+
+
+    if lst[2]['aantal'] > 0:
+        toppings[3]['prijs'] = 0.9
+    elif lst[3]['aantal'] > 0:
+        toppings[3]['prijs'] = 0.6
+
+    
+    for item in toppings:   
+        if item['aantal']: 
+            totaalPrijsToppings =+ item['aantal'] * item['prijs']
+
+    totaalPrijs += totaalPrijsToppings
+    
+    if totaalPrijsToppings:
+        print(f"topping               € {'%.2f' % totaalPrijsToppings}\n")
     print("-------------------------------")
-    print(f"Totaal                 € {'%.2f' % totaalPrijs}\n")
+    print(f"totaal                 € {'%.2f' % totaalPrijs}\n")
     print("Bedankt en tot ziens! ")
--- module_05-meer_fun(ctions)/proef_a-een_dry_ijssalon/ijssalon.py
+++ module_05-meer_fun(ctions)/proef_a-een_dry_ijssalon/ijssalon.py
@@ -8,28 +8,30 @@ lst = [{'name': 'bolletje', 'aantal': 0, 'prijs': 1.10},
         {'name': 'hoorntje', 'aantal': 0, 'prijs': 1.25},
         {'name': 'bakje', 'aantal': 0, 'prijs': 0.75}]
 
-smaakLst = [{'name': 'aardbei', 'aantal': 0, 'prijs': 1.10},
-             {'name': 'chocolade', 'aantal': 0, 'prijs': 1.10},
-             {'name': 'munt', 'aantal': 0, 'prijs': 1.10},
-             {'name': 'vanille', 'aantal': 0, 'prijs': 1.10}]
+smaakLst = [{'name': 'aardbei', 'aantal': 0, 'prijs': 1.10, 'key': 'a'},
+             {'name': 'chocolade', 'aantal': 0, 'prijs': 1.10, 'key': 'c'},
+             {'name': 'munt', 'aantal': 0, 'prijs': 1.10, 'key': 'm'},
+             {'name': 'vanille', 'aantal': 0, 'prijs': 1.10, 'key': 'a'}]
+
+toppingLst = [{'name': 'geen', 'aantal': 0, 'prijs': 0, 'key': 'a'},
+             {'name': 'slagroom', 'aantal': 0, 'prijs': 0.50, 'key': 'b'},
+             {'name': 'sprinkels', 'aantal': 0, 'prijs': 0.30, 'key': 'c'},
+             {'name': 'caramel saus', 'aantal': 0, 'prijs': 0, 'key': 'd'}]
+
+smaakTekst = f"Welke smaak wilt u voor bolletje? A) Aardbei, B) Chocolade, C) Munt of D) Vanille?\n "
+toppingTekst = "Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus?\n "
 
 while nogEen:
-    aantal, keuze = bakjeOfHoorntje(aantalBolletjes())
+    aantal = aantalBolletjes()
 
-    smaak = smaakKiezen(aantal)
+    keuze = bakjeOfHoorntje(aantal)
 
-    nogEen = meerBestellen(nogEen)
+    smaakEnTopping(aantal, smaakLst, smaakTekst)
 
-    tempLst = berekening(aantal, keuze)
+    smaakEnTopping(aantal, toppingLst, toppingTekst)
 
-    for item in tempLst:
-        for i, d in enumerate(lst):
-            if d['name'] == item['name']:
-                lst[i]['aantal'] += item['aantal']
+    nogEen = meerBestellen(nogEen)  
 
-    for item in smaak:
-        for i, d in enumerate(smaakLst):
-            if d['name'] == item['name']:
-                smaakLst[i]['aantal'] += item['aantal']
+    tempLst = bolletjesEnKeuzeBerekening(aantal, keuze, lst)
 
-bon(lst, smaakLst)
+bon(lst, smaakLst, toppingLst)
